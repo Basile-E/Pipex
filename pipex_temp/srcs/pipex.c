703adfd4a0f4aa6f1d14a49607b1c4359c7d8766
Basile Ecolivet <basile@Air-de-Basile.lan>
2025-04-29 20:48:54 +0200

    the final one ?

--- pipex_temp/srcs/pipex.c
+++ pipex_temp/srcs/pipex.c
@@ -28,15 +28,15 @@ char *Find_Path(char **ep)
         value = temp[1];
         if (ft_strncmp(key, "PATH", 4) == 0)
         {
-            ft_printf("key = %s\n\n", key);
-            ft_printf("value = %s\n\n", value);
-            ft_printf("PATH value is %s\n\n", value);
+            //ft_printf("key = %s\n\n", key);
+            //ft_printf("value = %s\n\n", value);
+            //ft_printf("PATH value is %s\n\n", value);
             break;
         }
         env_variable = ep[i];
         i++;
     }
-    ft_printf("valeur de i %i\n", i);
+    //ft_printf("valeur de i %i\n", i);
     return (value);
 }
 
@@ -46,65 +46,52 @@ int execute(char *path, char *av, char **ep)
     int i;
     char **exec_paths = ft_split(path, ':');
     i = 0;
-    ft_printf("valeur de i %i\n", i);
+    //ft_printf("valeur de i %i\n", i);
     while(exec_paths[i] != NULL)
     {
         char **cmd = ft_split(av, ' '); // separe la commande de ses params
         char *exec_temp = ft_strjoin(exec_paths[i], "/"); // prend le exec_path qu'on tente et lui ajoute '/'
         exec_temp = ft_strjoin(exec_temp, cmd[0]); // et ajoute la commande au bout du path pour voir si le fichier existe
-        ft_printf("exec_temp = %s\n", exec_temp);
+        //ft_printf("exec_temp = %s\n", exec_temp);
         if (access(exec_temp, F_OK) == 0) // veriffie que le programme existe ou non
         {
-            ft_printf("Executalble trouver = %s\n\n", exec_temp);
-            execve(exec_temp, cmd, ep); // execute le program trouver 
+            //ft_printf("Executalble trouver = %s\n\n", exec_temp);
+            execve(exec_temp, cmd, ep);
         }
         else
-            ft_printf("Exec non trouver on continue, i = %i\n", i++);
+            i++;
     }
 	return (0);
 }
 
-
 int	main(int ac, char **av, char **ep)
 {
 	int	fd[2];
-	int	pid;
+	pid_t	pid;
 	char *path;
-
-	// int fd_in = open(readonly)
-	// int fd_out = open(writeonly, create)
+	int fd_in = open(av[1], O_RDONLY);
+    int fd_out = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	
 	if (ac != 5)
 		return (ft_printf("Error: Wrong number of arg\n"));
 	if (pipe(fd) == -1)
 		return (ft_printf("Error: Pipe creation failed\n"));
-		//👮‍♂️ verification des arguments passez votre chemin 👮‍♂️
-	
-	path = Find_Path(ep);
-		
-	// il faut que :
-	// le process enfant ouvre le fichier1 et execute ls dessus (comment ?)
-	// puis redirige la sortie de ls (ls std output ?) ver pipe[1] (l'ecriture dans la pipe)
-	
-	// le processus parent ouvre ou crée le fichier2 et execute wc dessus (idem comment ?)
-	// puis redirige la sorti
-
-	
+    // si le program est trop long je peux faire une fonction pipex avec un copier coler de ce qu'il y a en dessous
+	path = Find_Path(ep);	
 	pid = fork();
 	if (pid == 0)
 	{
-		ft_printf("welcom from the child process\n");
+        dup2(fd_in, STDIN_FILENO);
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[0]);
-		ft_printf("av[2] = %s\n", av[2]);
 		execute(path, av[2], ep);
 	}
 	else if (pid > 0)
 	{
-		//gwait(NULL);
-		ft_printf("welcom from the parrent process\n");
+        dup2(fd_out, STDOUT_FILENO);
 		dup2(fd[0], STDIN_FILENO);
 		close(fd[1]);
 		execute(path, av[3], ep);
-	}	
+	}
+    return (0);
 }
